6e6fda15d6976c199cfcc88e6e03d1071de07c89
Marius22B <m02899402@gmail.com>
2026-08-18 10:57:58 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "barni-game-v3";
+const CACHE_NAME = "barni-game-v4";
 
 const FILES_TO_CACHE = [
     "./",
@@ -27,6 +27,15 @@ self.addEventListener("activate", event => {
             )
         ).then(() => self.clients.claim())
     );
+});
+
+self.addEventListener("message", event => {
+    if (
+        event.data &&
+        event.data.type === "SKIP_WAITING"
+    ) {
+        self.skipWaiting();
+    }
 });
 
 self.addEventListener("fetch", event => {
